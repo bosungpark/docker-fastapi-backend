f90201966e6f3dc3f5cac6ea812b300966d3e753
bosungpark <qkrqhtjd0806@naver.com>
2023-06-02 20:42:09 +0900

    fix logix

--- chat_server_1/source/main.py
+++ chat_server_1/source/main.py
@@ -21,9 +21,9 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var client_id = Date.now()
+            var client_id = 1
             document.querySelector("#ws-id").textContent = client_id;
-            var ws = new WebSocket(`ws://localhost:8001/ws/${client_id}`);
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
